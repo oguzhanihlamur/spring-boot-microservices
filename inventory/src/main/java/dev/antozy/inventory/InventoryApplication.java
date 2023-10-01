df9cf84b4ebc77e214e9cf8da59dd7842b3da8fa
oguzhanihlamur <oguzhanihlamur@gmail.com>
2023-10-02 00:17:34 +0300

    First step is done
    In this commit, we communicate the services with each other. We write and read to database and kafka. We use PostgreSQL as database and we use Upstash as Kafka servers.

--- inventory/src/main/java/dev/antozy/inventory/InventoryApplication.java
+++ inventory/src/main/java/dev/antozy/inventory/InventoryApplication.java
@@ -1,7 +1,21 @@
 package dev.antozy.inventory;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Optional;
 
 @SpringBootApplication
 public class InventoryApplication {
@@ -9,5 +23,49 @@ public class InventoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
+
+}
+
+@RestController
+@RequiredArgsConstructor
+class InventoryController {
+
+	private final ProductRepository productRepository;
+
+//	private Map<String, InventoryStatus> statuses = Map.of("1", new InventoryStatus(true), "2", new InventoryStatus(false));
+	@GetMapping("inventories")
+	public InventoryStatus getInventory(@RequestParam String productId){
+		InventoryStatus inventoryStatus = new InventoryStatus();
+		Optional<Product> order = productRepository.findById(Long.parseLong(productId));
+		if(order.isPresent()) {
+			inventoryStatus.setExists(true);
+		} else {
+			inventoryStatus.setExists(false);
+		}
+		return inventoryStatus;
+	}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class InventoryStatus {
+	private boolean exists;
+}
+
+@Entity(name = "product")
+@Data
+class Product {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String product;
+	private double price;
+
+}
+
+interface ProductRepository extends CrudRepository<Product, Long> {
 
 }
--- order/src/main/java/dev/antozy/order/OrderApplication.java
+++ order/src/main/java/dev/antozy/order/OrderApplication.java
@@ -1,13 +1,121 @@
 package dev.antozy.order;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequiredArgsConstructor
+class OrderController {
+
+	private final OrderService orderService;
+
+	@PostMapping("/orders")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void placeOrder(@RequestBody PlaceOrderRequest request) {
+		this.orderService.placeOrder(request);
+	}
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class PlaceOrderRequest {
+	private String product;
+	private double price;
+}
+
+@Service
+@RequiredArgsConstructor
+class OrderService {
+
+	private final KafkaTemplate kafkaTemplate;
+	private final OrderRepository orderRepository;
+	private final InventoryClient inventoryClient;
+
+	public void placeOrder(PlaceOrderRequest request) {
+//		System.out.println("Order placed for : " + request.getProduct() + " - " + request.getPrice());
+	InventoryStatus status = inventoryClient.exists(request.getProduct());
+		if (!status.isExists()) {
+			throw new EntityNotFoundException("Product does not exists");
+		}
+
+		Order order = new Order();
+		order.setProduct(request.getProduct());
+		order.setPrice(request.getPrice());
+		order.setStatus("PLACED");
+		Order orderK = this.orderRepository.save(order);
+		this.kafkaTemplate.send("prod.orders.placed", String.valueOf(orderK.getId()), OrderPlacedEvent.builder()
+				.product(request.getProduct())
+				.price(request.getPrice())
+				.orderId(order.getId().intValue())
+				.build());
+	}
+
+	@KafkaListener(topics = "prod.orders.shipped", groupId = "order-group")
+	public void handleOrderShippingEvent(String orderId) {
+		this.orderRepository.findById(Long.valueOf(orderId)).ifPresent(order -> {
+			order.setStatus("SHIPPED");
+			this.orderRepository.save(order);
+		});
+	}
+}
+
+@Data
+@Builder
+class OrderPlacedEvent {
+	private int orderId;
+	private String product;
+	private double price;
+}
+
+interface OrderRepository extends CrudRepository<Order, Long> {
+
+}
+
+@Entity(name = "orders")
+@Data
+class Order {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String product;
+	private double price;
+
+	private String status;
+}
+
+@FeignClient(url = "http://localhost:8092", name = "inventories")
+interface InventoryClient {
+	@GetMapping("/inventories")
+	InventoryStatus exists(@RequestParam("productId") String productId);
+}
+
+@Data
+class InventoryStatus {
+	private boolean exists;
 }
